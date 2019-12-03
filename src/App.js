7c192fe13d4502e60e0d8f93301aeefd8b1d8c80
DBasias <dbasiadimas@hotmail.com>
2019-12-03 18:58:51 +0200

    connected app with clarify and displaying image in FaceRecognition component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import Clarifai from "clarifai";
+
 import Particles from "react-particles-js";
 import Navigation from "./components/Navigation";
 import Logo from "./components/Logo";
 import ImageLinkForm from "./components/ImageLinkForm";
 import Rank from "./components/Rank";
+import FaceRecognition from "./components/FaceRecognition";
+
 import "./App.css";
+
+const app = new Clarifai.App({
+  apiKey: process.env.REACT_APP_CLARIFAI
+});
 
 const particlesOptions = {
   particles: {
@@ -18,17 +26,48 @@ const particlesOptions = {
   }
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particles" params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageURL: ""
+    };
+  }
+
+  onInputChange = e => {
+    this.setState({ input: e.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageURL: this.state.input });
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageURL={this.state.imageURL} />
+      </div>
+    );
+  }
 }
 
 export default App;
